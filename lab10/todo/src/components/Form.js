81197d57cb5c93859f6aff133952dfbd98c8623a
Endrju00 <andrzej.kapczynski21@gmail.com>
2022-05-14 16:04:18 +0200

    Add React Todo App

---
+++ lab10/todo/src/components/Form.js
@@ -0,0 +1,36 @@
+const Form = ({ setInputText, todos, setTodos, inputText, setStatus }) => {
+    const inputTextHandler = (event) => {
+        setInputText(event.target.value);
+    };
+
+    const submitTodoHandler = (event) => {
+        event.preventDefault();
+        setTodos([
+            ...todos,
+            { text: inputText, completed: false, id: Math.random() * 1000 }
+        ]);
+        setInputText("");
+    }
+
+    const statusHandler = (event) => {
+        setStatus(event.target.value);
+    }
+
+    return (
+        <form>
+            <input value={inputText} onChange={inputTextHandler} type="text" className="todo-input" />
+            <button onClick={submitTodoHandler} className="todo-button" type="submit">
+                <i className="fas fa-plus-square"></i>
+            </button>
+            <div className="select">
+                <select onChange={statusHandler} name="todos" className="filter-todo">
+                    <option value="all">All</option>
+                    <option value="completed">Completed</option>
+                    <option value="uncompleted">Uncompleted</option>
+                </select>
+            </div>
+        </form>
+    );
+};
+
+export default Form;
